6032b8752bf6c75cb3bcbdb89baee931a0ba0481
Simona Nikolova <simona.nikolova@vero-digital.solutions>
2024-01-18 11:07:24 +0200

    Add Kdoc to data class

--- android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/annotation/core/pdf/PdfToImageResultData.kt
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/annotation/core/pdf/PdfToImageResultData.kt
@@ -2,6 +2,12 @@ package com.github.barteksc.pdfviewer.annotation.core.pdf
 
 import java.io.File
 
+/** An instance of this class is created when we annotate the PDF.
+ * @param pdfFile - the pdf file we want to annotate
+ * @param imageFile - the PNG image that is created from the given PDF file's first page,
+ * this image will be passed to MeasureLib
+ * @param pageHeight - the height of the PDF page/image, needed when converting between PDF and image space
+ * @param jsonShapes - the PDF annotations mapped to json shapes, that will be passed to MeasureLib */
 data class PdfToImageResultData(
     val pdfFile: File,
     val imageFile: File,
