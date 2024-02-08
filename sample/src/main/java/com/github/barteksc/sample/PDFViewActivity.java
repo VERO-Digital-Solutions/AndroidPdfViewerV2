797b4c977f1cd40125bcd044212a7c14f489e684
Simona Nikolova <simona.nikolova@vero-digital.solutions>
2024-02-08 19:47:42 +0200

    Change parameter for sample activity

--- sample/src/main/java/com/github/barteksc/sample/PDFViewActivity.java
+++ sample/src/main/java/com/github/barteksc/sample/PDFViewActivity.java
@@ -178,7 +178,7 @@ public class PDFViewActivity extends AppCompatActivity implements OnPageChangeLi
                         .onLoad(this)
                         .enableSwipe(true)
                         .scrollHandle(new DefaultScrollHandle(this))
-                        .linkHandler(pdfFile.getAbsolutePath(), () -> logDebug("MAIN", "Runnable works"))
+                        .linkHandler(pdfFile.getAbsolutePath(), documentationId -> {})
                         .spacing(10) // in dp
                         .onPageError(this)
                         .onTap(this)
