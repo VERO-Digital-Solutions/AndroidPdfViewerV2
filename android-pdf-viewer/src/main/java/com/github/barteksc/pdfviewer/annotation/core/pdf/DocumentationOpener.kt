0e008485f4d81b673a47077c7a6e720b532fcf30
Simona Nikolova <simona.nikolova@vero-digital.solutions>
2024-02-09 11:53:12 +0200

    Rename parameter

--- android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/annotation/core/pdf/DocumentationOpener.kt
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/annotation/core/pdf/DocumentationOpener.kt
@@ -1,5 +1,5 @@
 package com.github.barteksc.pdfviewer.annotation.core.pdf
 
 interface DocumentationOpener {
-    fun openDocumentation(schemaId: Long, documentationID: String?)
+    fun openDocumentation(schemaId: Long, documentId: String?)
 }
